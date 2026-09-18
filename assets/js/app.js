a2990e85393891cf101dd68f61b01a05008bef13
LONEmi20 <micgredy@gmail.com>
2026-09-18 09:17:24 +0700

    update minor 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,8 +70,6 @@ const CONFIG = {
             container.innerHTML = ''; 
 
             const categories = ['Coffee', 'Non Coffee', 'Japanese Coffee'];
-            const icons = {'Coffee': '😃', 'Non Coffee': '😃', 'Japanese Coffee': '😃'};
-
             categories.forEach((cat, index) => {
                 const categoryProducts = products.filter(p => p.category === cat);
                 if (categoryProducts.length > 0) {
